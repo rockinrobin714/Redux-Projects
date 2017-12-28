122a097ef03b4813ac4559cd3715ba313e4e6fac
rockinrobin714 <robindykema@gmail.com>
2017-12-27 23:35:57 -0600

    Finished book list

--- book_list/src/reducers/reducer_books.js
+++ book_list/src/reducers/reducer_books.js
@@ -1,9 +1,10 @@
 export default function() {
   return [
-    { title: 'Harry Potter' },
-    { title: 'Little House on the Prairie' },
-    { title: 'The Hunger Games' },
-    { title: 'The Dark Tower' },
-    { title: 'The Bible' },
+    { title: 'Harry Potter', pages: 123 },
+    { title: 'Little House on the Prairie', pages: 456 },
+    { title: 'The Hunger Games', pages: 12 },
+    { title: 'The Dark Tower', pages: 412},
+    { title: 'Lord Of the Rings', pages: 1 },
+    { title: 'The Bible', pages: 12354 },
   ]
 }
